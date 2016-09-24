2c7ea97d44403a02448085a38533b081715fae63
Jeremy J Starcher <GeekAthelon@gmail.com>
2016-09-24 10:47:45 -0400

    Resolved issue of not being able to see homepages properly by fixing all the broken URLs.

--- trunk/src/taillist.js
+++ trunk/src/taillist.js
@@ -6,8 +6,6 @@ var tailList = {
       "mainRoom": "soi",
       "hotRoom": "hotlist",
       "gossipRoom": "babble",
-      "hotUrl": "https://soi.hyperchat.com/cgi-bin/soi.cgi?EParms=ru|kqw29tRmwmpkv%22ru|vk9g/Gkjpvkh/GajpvaDwkm%22ru|le9Rmwmpkv&vqvab=soi@soi&vqvaa=hotlist&vqxti=1219268970#newtalk",
-      "SeeAllUrl": "https://ssom10.hyperchat.com/cgi-bin/som10.cgi?EParms=ru|kqw29tRmwmpkv%22ru|vk9feffhaDwkm%22ru|le9Rmwmpkv&vqvab=soi@soi&vqxti=1219269053#newtalk",
       "name": "State of insanity",
       "background": "/state/frgback.jpg",
       "roomBase": "https://soiroom.hyperchat.com/!!!/",
@@ -18,92 +16,66 @@ var tailList = {
       "mainRoom": "beware",
       "hotRoom": "hotlist",
       "gossipRoom": "gossip",
-      "hotUrl": "https://sbwr.hyperchat.com/newchat-cgi/bwr.cgi?vqxus=Visitor&vqxro=c+Control+Centre@bwr&vqxha=Visitor&vqvab=beware@bwr&vqvaa=hotlist&vqxti=1219269328#newtalk",
-      "SeeAllUrl": "https://sbwr.hyperchat.com/newchat-cgi/bwr.cgi?vqxus=Visitor&vqxro=gossip@bwr&vqxha=Visitor&vqvab=beware@bwr&vqxti=1219269220#newtalk",
       "name": "Beware Castle",
       "background": "/bwr/defback.jpg",
-      "roomBase": "",
-      "userBase": "https://!!!.castlebeware.com/"
+      "roomBase": "http://bwr.hyperchat.com/newchat/r/!!!/~bwr/",
+      "userBase": "https://bwr.hyperchat.com/newchat/u/!!!/~bwr/",
     },
     "jag": {
       "url": "https://jag.hyperchat.com/",
       "mainRoom": "jaghall",
       "hotRoom": "hotlist",
       "gossipRoom": "seeall",
-      "hotUrl": "https://jag.hyperchat.com/newchat-cgi/jag.cgi?vqxus=Visitor&vqxro=c+Control+Centre@jag&vqxha=Visitor&vqvab=jaghall@jag&vqvaa=hotlist&vqxti=1219269469#newtalk",
-      "SeeAllUrl": "https://sbwr.hyperchat.com/newchat-cgi/bwr.cgi?vqxus=Visitor&vqxro=gossip@bwr&vqxha=Visitor&vqvab=beware@bwr&vqxti=1219269220#newtalk",
       "name": "Justice Alliance of Gor",
       "background": "/jag/defback.jpg",
       "roomBase": "https://jagroom.hyperchat.com/!!!/",
-      "userBase": "https://!!!.jaghome.com/"
+      "userBase": "https://jaguser.hyperchat.com/!!!/"
     },
     "gor": {
       "url": "https://gor.hyperchat.com/",
       "mainRoom": "gor",
       "hotRoom": "hotlist",
       "gossipRoom": "seeall",
-      "hotUrl": "https://gor.hyperchat.com/newchat-cgi/gor.cgi?vqxus=Visitor&vqxro=c+Control+Centre@gor&vqxha=Visitor&vqvab=m@gor&vqvaa=hotlist&vqxti=1219270100#newtalk",
-      "SeeAllUrl": "https://gor.hyperchat.com/newchat-cgi/gor.cgi?vqxus=Visitor&vqxro=seeall@gor&vqxha=Visitor&vqvab=m@gor&vqxti=1219270120#newtalk",
       "name": "The Gorean Realm",
       "background": "/gor/defback.jpg",
-      "roomBase": "https://gor.hyperchat.com/newchat/r/!!!/~gor/",
-      "userBase": "https://!!!.realgor.com/"
+      "roomBase": "http://gor.hyperchat.com/newchat/r/!!!/~gor/",
+      "userBase": "http://gor.hyperchat.com/newchat/u/!!!/~gor/"
     },
     "ns": {
       "url": "https://ns.hyperchat.com/",
       "mainRoom": "homeroom",
       "hotRoom": "hotlist",
       "gossipRoom": "seeall",
-      "hotUrl": "https://ns.hyperchat.com/newchat-cgi/ns.cgi?vqxus=Visitor&vqxro=c+Control+Centre@ns&vqxha=Visitor&vqvab=m@ns&vqvaa=hotlist&vqxti=1219270287#newtalk",
-      "SeeAllUrl": "https://ns.hyperchat.com/newchat-cgi/ns.cgi?vqxus=Visitor&vqxro=seeall@ns&vqxha=Visitor&vqvab=m@ns&vqxti=1219270217#newtalk",
       "name": "The Night Sky",
       "background": "/ns/frgback.jpg",
-      "roomBase": "https://nsroom.hyperchat.com/!!!/",
-      "userBase": ""
-    },
-    "my": {
-      "url": "https://medievalyore.com/",
-      "mainRoom": "tavern",
-      "hotRoom": "hotlist",
-      "gossipRoom": "seeall",
-      "SeeAllUrl": "https://myore.hyperchat.com/newchat-cgi/my.cgi?vqxus=Visitor&vqxro=seeall@my&vqxha=Visitor&vqvab=tavern@my&vqxti=1219267293#newtalk",
-      "hotUrl": "https://myore.hyperchat.com/newchat-cgi/my.cgi?vqxus=Visitor&vqxro=c+Control+Centre@my&vqxha=Visitor&vqvab=tavern@my&vqvaa=hotlist&vqxti=1219267323#newtalk",
-      "name": "Medieval Yore",
-      "background": "/frgback.jpg",
-      "roomBase": "https://room.medievalyore.com/!!!/",
-      "userBase": "https://!!!.medievalyore.com/"
+      "roomBase": "https://sns.hyperchat.com/newchat/r/~ns/!!!/",
+      "userBase": "https://sns.hyperchat.com/newchat/u/~ns/!!!/"
     },
     "soe": {
       "url": "https://soe.hyperchat.com/",
       "mainRoom": "honor",
       "hotRoom": "hotlist",
       "gossipRoom": "view",
-      "SeeAllUrl": "https://soe.hyperchat.com/newchat-cgi/soe.cgi?vqxus=Visitor&vqxro=view@soe&vqxha=Visitor&vqvab=honor@soe&vqxti=1219270685#newtalk",
-      "hotUrl": "https://soe.hyperchat.com/newchat-cgi/soe.cgi?vqxus=Visitor&vqxro=c+Control+Centre@soe&vqxha=Visitor&vqvab=honor@soe&vqvaa=hotlist&vqxti=1219270690#newtalk",
       "name": "State of Enchantment",
       "background": "soe/defback.jpg",
-      "roomBase": "https://soe.hyperchat.com/newchat/r/!!!/~soe/",
-      "userBase": "https://soe.hyperchat.com/newchat/u/!!!/~soe/"
+      "roomBase": "https://ssoe.hyperchat.com/newchat/r/~soe/!!!/",
+      "userBase": "https://ssoe.hyperchat.com/newchat/u/~soe/!!!/"
     },
     "mansion": {
       "url": "https://mansion.hyperchat.com/",
       "mainRoom": "lobby",
       "hotRoom": "hotlist",
       "gossipRoom": "talking",
-      "SeeAllUrl": "https://mansion.hyperchat.com/newchat-cgi/mansion.cgi?vqxus=Visitor&vqxro=talking@mansion&vqxha=Visitor&vqvab=lobby@mansion&vqxti=1219270780#newtalk",
-      "hotUrl": "https://mansion.hyperchat.com/newchat-cgi/mansion.cgi?vqxus=Visitor&vqxro=c+Control+Centre@mansion&vqxha=Visitor&vqvab=lobby@mansion&vqvaa=hotlist&vqxti=1219270798#newtalk",
       "name": "The Mansion",
       "background": "/mansion/defback.jpg",
-      "roomBase": "https://mansion.hyperchat.com/newchat/r/!!!/~mansion/",
-      "userBase": "https://mansion.hyperchat.com/newchat/u/!!!/~mansion/"
+      "roomBase": "https://smansion.hyperchat.com/newchat/r/!!!/~mansion/",
+      "userBase": "https://smansion.hyperchat.com/newchat/u/!!!/~mansion/"
     },
     "isle": {
       "url": "https://isle.hyperchat.com/",
       "mainRoom": "tc",
       "hotRoom": "hotlist",
       "gossipRoom": "seeall",
-      "SeeAllUrl": "https://isle.hyperchat.com/newchat-cgi/isle.cgi?vqxus=Visitor&vqxro=seeall@isle&vqxha=Visitor&vqvab=tc@isle&vqxti=1219270873#newtalk",
-      "hotUrl": "https://isle.hyperchat.com/newchat-cgi/isle.cgi?vqxus=Visitor&vqxro=c+Control+Centre@isle&vqxha=Visitor&vqvab=tc@isle&vqvaa=hotlist&vqxti=1219270939#newtalk",
       "name": "Isle of Pain and Pleasure",
       "background": "/isle/defback.jpg",
       "roomBase": "https://isleroom.hyperchat.com/!!!/",
@@ -114,24 +86,20 @@ var tailList = {
       "mainRoom": "labrys",
       "hotRoom": "hotlist",
       "gossipRoom": "loft",
-      "SeeAllUrl": "https://resort.hyperchat.com/newchat-cgi/resort.cgi?vqxus=Visitor&vqxro=loft@resort&vqxha=Visitor&vqvab=labrys@resort&vqxti=1219271019#newtalk",
-      "hotUrl": "https://resort.hyperchat.com/newchat-cgi/resort.cgi?vqxus=Visitor&vqxro=c+Control+Centre@resort&vqxha=Visitor&vqvab=labrys@resort&vqvaa=hotlist&vqxti=1219271069#newtalk",
       "name": "The Rainbow Resort",
       "background": "/resort/defback.jpg",
-      "roomBase": "https://resort.hyperchat.com/newchat/r/!!!/~resort/",
-      "userBase": "https://resort.hyperchat.com/newchat/u/!!!/~resort/"
+      "roomBase": "https://sresort.hyperchat.com/newchat/r/!!!/~resort/",
+      "userBase": "https://sresort.hyperchat.com/newchat/r/!!!/~resort/"
     },
     "war": {
       "url": "https://www.war-site.com/",
       "mainRoom": "war",
       "hotRoom": "hotlist",
       "gossipRoom": "seeall",
-      "SeeAllUrl": "https://www.war-site.com/newchat-cgi/war.cgi?vqxus=Visitor&vqxro=seeall@war&vqxha=Visitor&vqvab=war@war&vqxti=1219272545#newtalk",
-      "hotUrl": "https://www.war-site.com/newchat-cgi/war.cgi?vqxus=Visitor&vqxro=c+Control+Centre@war&vqxha=Visitor&vqvab=war@war&vqvaa=hotlist&vqxti=1219272570#newtalk",
       "name": "The Warriors Realm",
       "background": "/defback.jpg",
-      "roomBase": "https://room.war-site.com/!!!/",
-      "userBase": "https://!!!.war-site.com"
+      "roomBase": "https://warroom.hyperchat.com/!!!/",
+      "userBase": "https://waruser.hyperchat.com/!!!/"
     },
     "waw": {
       "url": "https://waw.hyperchat.com/",
@@ -143,6 +111,6 @@ var tailList = {
       "name": "Witches and Wizards RPG",
       "background": "/waw/frgback.jpg",
       "roomBase": "https://wawroom.hyperchat.com/!!!/",
-      "userBase": "https://!!!.wawhome.com/"
+      "userBase": "https://wawuser.hyperchat.com/!!!/"
     }
   };
